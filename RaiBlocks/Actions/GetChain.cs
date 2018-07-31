9e06519fa8af350a0dc4a500c123498fac25c8f9
bkarabanov <bkarabanov@ics.perm.ru>
2018-07-31 17:47:01 +0500

    Implement blocks info action.

---
+++ RaiBlocks/Actions/GetChain.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using RaiBlocks.Interfaces;
+using RaiBlocks.Results;
+
+namespace RaiBlocks.Actions
+{
+    public class GetChain: IAction<GetChainResult>
+    {
+        [JsonProperty("action")]
+        public string Action { get; } = "chain";
+        
+        [JsonProperty("block")]
+        public string Block { get; set; }
+        
+        [JsonProperty("count")]
+        public long Count { get; set; }
+    }
+}
---
+++ RaiBlocks/Results/GetChainResult.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using RaiBlocks.ValueObjects;
+
+namespace RaiBlocks.Results
+{
+    public class GetChainResult
+    {
+        [JsonProperty("blocks")]
+        public List<string> Blocks { get; set; }
+    }
+}
